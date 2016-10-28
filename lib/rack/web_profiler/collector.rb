41907cb704a39c5aa57a9a16dd7360cd603b25ca
Nicolas Brousse <nicolas@pantographe.studio>
2016-10-28 04:44:13 +0200

    Move collector view in collector file

--- lib/rack/web_profiler/collector.rb
+++ lib/rack/web_profiler/collector.rb
@@ -1,172 +1,210 @@
 require "docile"
 
 module Rack
-
-  # Collector
-  class WebProfiler::Collector
-    autoload :View, "rack/web_profiler/collector/view"
-
-    # Collectors
-    autoload :RackCollector,      "rack/web_profiler/collector/rack_collector"
-    autoload :RequestCollector,   "rack/web_profiler/collector/request_collector"
-    autoload :RubyCollector,      "rack/web_profiler/collector/ruby_collector"
-    autoload :TimeCollector,      "rack/web_profiler/collector/time_collector"
-
-    # DSL
-    module DSL
-      def self.included(base)
-        base.extend(ClassMethods)
-        base.class_eval do
-          @definition            = Definition.new
-          @definition.position   = 1
-          @definition.is_enabled = true
-          @definition.klass      = self
-        end
-      end
-
-      module ClassMethods
-        attr_reader :definition
-
-        # Set the icon of the {Rack::WebProfiler::Collector}.
-        #
-        # @param icon [String, nil]
-        def icon(icon = nil); definition.icon = icon; end
-
-        # Set the name of the {Rack::WebProfiler::Collector}.
-        #
-        # @param name [String, nil]
-        def collector_name(name = nil); definition.name = name; end
-
-        #
-        def position(position = nil); definition.position = position.to_i; end
-
-        #
-        def collect(&block); definition.collect = block; end
-
-        # Set the template of the {Rack::WebProfiler::Collector}.
-        #
-        # @param template [String, nil]
-        # @option type [Symbol] :file or :DATA
-        def template(template = nil, type: :file)
-          template = get_data_contents(template) if type == :DATA
-          definition.template = template
-        end
-
-        # Tell if the {Rack::WebProfiler::Collector} is enabled.
-        #
-        # @param is_enabled [Boolean, Block]
-        def is_enabled?(is_enabled = true)
-          definition.is_enabled = Proc.new if block_given?
-          definition.is_enabled = is_enabled
-        end
-
-        private
-
-        def get_data_contents(path)
-          data = ""
-          ::File.open(path, "rb") do |f|
-            begin
-              line = f.gets
-            end until line.nil? || /^__END__$/ === line
-            data << line while line = f.gets
+  class WebProfiler
+    # Collector
+    class Collector
+      # DSL
+      module DSL
+        def self.included(base)
+          base.extend(ClassMethods)
+          base.class_eval do
+            @definition            = Definition.new
+            @definition.position   = 1
+            @definition.is_enabled = true
+            @definition.klass      = self
           end
-          data
+        end
+
+        module ClassMethods
+          attr_reader :definition
+
+          # Set the icon of the {Rack::WebProfiler::Collector}.
+          #
+          # @param icon [String, nil]
+          def icon(icon = nil); definition.icon = icon; end
+
+          # Set the name of the {Rack::WebProfiler::Collector}.
+          #
+          # @param name [String, nil]
+          def collector_name(name = nil); definition.name = name; end
+
+          #
+          def position(position = nil); definition.position = position.to_i; end
+
+          #
+          def collect(&block); definition.collect = block; end
+
+          # Set the template of the {Rack::WebProfiler::Collector}.
+          #
+          # @param template [String, nil]
+          # @option type [Symbol] :file or :DATA
+          def template(template = nil, type: :file)
+            template = get_data_contents(template) if type == :DATA
+            definition.template = template
+          end
+
+          # Tell if the {Rack::WebProfiler::Collector} is enabled.
+          #
+          # @param is_enabled [Boolean, Block]
+          def is_enabled?(is_enabled = true)
+            definition.is_enabled = Proc.new if block_given?
+            definition.is_enabled = is_enabled
+          end
+
+          private
+
+          def get_data_contents(path)
+            data = ""
+            ::File.open(path, "rb") do |f|
+              begin
+                line = f.gets
+              end until line.nil? || /^__END__$/ === line
+              data << line while line = f.gets
+            end
+            data
+          end
         end
       end
-    end
 
-    # Definition
-    #
-    # Collector definition.
-    class Definition
-      attr_accessor :icon, :name, :position, :collect, :template, :is_enabled, :klass
-      attr_reader   :data_storage
+      # Definition
+      #
+      # Collector definition.
+      class Definition
+        attr_accessor :icon, :name, :position, :collect, :template, :is_enabled, :klass
+        attr_reader   :data_storage
 
-      # Collect the data who the Collector need.
-      #
-      # @param request [Rack::WebProfiler::Request]
-      # @param response [Rack::Response]
-      #
-      # @return [Rack::WebProfiler::Collector::DSL::DataStorage]
-      def collect!(request, response)
-        @data_storage = Docile.dsl_eval(DataStorage.new, request, response, &collect)
+        # Collect the data who the Collector need.
+        #
+        # @param request [Rack::WebProfiler::Request]
+        # @param response [Rack::Response]
+        #
+        # @return [Rack::WebProfiler::Collector::DSL::DataStorage]
+        def collect!(request, response)
+          @data_storage = Docile.dsl_eval(DataStorage.new, request, response, &collect)
+        end
+
+        # Is the collector enabled.
+        #
+        # @return [Boolean]
+        def is_enabled?
+          return !!@is_enabled.call if @is_enabled.is_a?(Proc)
+          !!@is_enabled
+        end
       end
 
-      # Is the collector enabled.
+      # DataStorage
       #
-      # @return [Boolean]
-      def is_enabled?
-        return !!@is_enabled.call if @is_enabled.is_a?(Proc)
-        !!@is_enabled
+      # Used to store datas who Collector needs.
+      #
+      # @todo do DataStorage compatible with Marshal
+      class DataStorage
+        attr_reader :datas
+
+        def initialize
+          @datas      = Hash.new
+          @status     = nil
+          @show_tab   = true
+          @show_panel = true
+        end
+
+        # Store a value.
+        #
+        # @param k [String, Symbol]
+        # @param v
+        def store(k, v)
+          # @todo check data format (must be compatible with Marshal)
+          @datas[k.to_sym] = v
+        end
+
+        # Status.
+        #
+        # @param v [Symbol, nil]
+        #
+        # @return [Symbol, nil]
+        def status(v = nil)
+          # @todo check status?
+          # raise Exception, "" unless [:success, :warning, :danger].include?(v)
+          @status = v.to_sym unless v.nil?
+          @status
+        end
+
+        #
+        #
+        # @param b [Boolean, nil]
+        #
+        # @return [Boolean]
+        def show_panel(b = nil)
+          @show_panel = !!b unless b.nil?
+          @show_panel
+        end
+
+        #
+        #
+        # @param b [Boolean, nil]
+        #
+        # @return [Boolean]
+        def show_tab(b = nil)
+          @show_tab = !!b unless b.nil?
+          @show_tab
+        end
+
+        # Transform DataStorage to an Hash
+        #
+        # @return [Hash<Symbol, Object>]
+        def to_h
+          {
+            datas:      @datas,
+            status:     @status,
+            show_panel: @show_panel,
+            show_tab:   @show_tab,
+          }
+        end
       end
-    end
 
-    # DataStorage
-    #
-    # Used to store datas who Collector needs.
-    #
-    # @todo do DataStorage compatible with Marshal
-    class DataStorage
-      attr_reader :datas
+      # View
+      class View < WebProfiler::View
+        #
+        def context
+          @context ||= Context.new
+        end
 
-      def initialize
-        @datas      = Hash.new
-        @status     = nil
-        @show_tab   = true
-        @show_panel = true
-      end
+        # Helpers
+        module Helpers
+          # @todo comment
+          def tab_content
+            if block_given?
+              @tab_content ||= capture(&Proc.new)
+            elsif !@tab_content.nil?
+              @tab_content
+            end
+          end
 
-      # Store a value.
-      #
-      # @param k [String, Symbol]
-      # @param v
-      def store(k, v)
-        # @todo check data format (must be compatible with Marshal)
-        @datas[k.to_sym] = v
-      end
+          # @todo comment
+          def panel_content
+            if block_given?
+              @panel_content ||= capture(&Proc.new)
+            elsif !@panel_content.nil?
+              @panel_content
+            end
+          end
 
-      # Status.
-      #
-      # @param v [Symbol, nil]
-      #
-      # @return [Symbol, nil]
-      def status(v = nil)
-        # @todo check status?
-        # raise Exception, "" unless [:success, :warning, :danger].include?(v)
-        @status = v.to_sym unless v.nil?
-        @status
-      end
+          # @todo comment
+          def data(k, default = nil)
+            return nil if @collection.nil?
 
-      #
-      #
-      # @param b [Boolean, nil]
-      #
-      # @return [Boolean]
-      def show_panel(b = nil)
-        @show_panel = !!b unless b.nil?
-        @show_panel
-      end
+            datas = @collection.datas[@collector.name.to_sym][:datas]
+            return datas[k] if datas.has_key?(k)
 
-      #
-      #
-      # @param b [Boolean, nil]
-      #
-      # @return [Boolean]
-      def show_tab(b = nil)
-        @show_tab = !!b unless b.nil?
-        @show_tab
-      end
+            default
+          end
+        end
 
-      # Transform DataStorage to an Hash
-      #
-      # @return [Hash<Symbol, Object>]
-      def to_h
-        {
-          datas:      @datas,
-          status:     @status,
-          show_panel: @show_panel,
-          show_tab:   @show_tab,
-        }
+        # @todo comment
+        class Context
+          include WebProfiler::View::Helpers::Common
+          include Helpers
+        end
       end
     end
   end
