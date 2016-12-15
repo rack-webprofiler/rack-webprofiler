bac3c3b5bbf98fd1a1763b6ce8c9ad5894210123
Nicolas Brousse <nicolas@pantographe.studio>
2016-12-15 18:32:43 +0100

    Move CollectionRecord in Model class

--- lib/rack/web_profiler/model.rb
+++ lib/rack/web_profiler/model.rb
@@ -1,36 +1,85 @@
 require "sequel"
 
 module Rack
-  # Model
-  class WebProfiler::Model
-    autoload :CollectionRecord, "rack/web_profiler/model/collection_record"
+  class WebProfiler
+    # Model
+    class Model
+      class << self
+        # Get the WebProfiler database.
+        #
+        # @return [Sequel::SQLite::Database]
+        def database
+          @db ||= Sequel.connect("sqlite://#{db_file_path}", {
+            single_threaded: true,
+            # timeout: 5000,
+            # # single_threaded: true, # = mieux
+            # pool_timeout: 5000,
+            # max_connections: 1,
+          })
+        end
 
-    class << self
-      # Get the WebProfiler database.
-      #
-      # @return [Sequel::SQLite::Database]
-      def database
-        @db ||= Sequel.connect("sqlite://#{db_file_path}", {
-          single_threaded: true,
-        })
+        # Remove the database content.
+        def clean!
+          return unless ::File.exist?(db_file_path)
+
+          ::File.delete(db_file_path)
+          @db = nil
+          @db_file_path = nil
+        end
+
+        private
+
+        # Returns the db file path.
+        #
+        # @return [String]
+        def db_file_path
+          @db_file_path ||= ::File.join(WebProfiler.config.tmp_dir, "rack-webprofiler.db")
+        end
       end
 
-      # Remove the database content.
-      def clean!
-        return unless ::File.exist?(db_file_path)
+      class CollectionRecord < Sequel::Model(Rack::WebProfiler::Model.database)
+        # Plugins.
+        plugin :schema
+        plugin :hook_class_methods
+        plugin :serialization
+        plugin :json_serializer
+        plugin :timestamps
 
-        ::File.delete(db_file_path)
-        @db = nil
-        @db_file_path = nil
-      end
+        # Attributes:
+        #   - id
+        #   - token
+        #   - ip
+        #   - url
+        #   - http_method
+        #   - http_status
+        #   - content_type
+        #   - datas
+        #   - created_at
+        set_schema do
+          primary_key :id
 
-      private
+          varchar  :token,       unique: true, empty: false
+          varchar  :url,         empty: false
+          varchar  :ip,          empty: false
+          varchar  :http_method, empty: false
+          integer  :http_status
+          varchar  :content_type
+          string   :datas,       text: true, empty: false
+          datetime :created_at
+        end
+        create_table unless table_exists?
 
-      # Returns the db file path.
-      #
-      # @return [String]
-      def db_file_path
-        @db_file_path ||= ::File.join(WebProfiler.config.tmp_dir, "rack-webprofiler.db")
+        # Serialization.
+        serialize_attributes :marshal, :datas
+
+        # Hooks.
+        before_create :before_create
+
+        # Generate a token to the record before create it.
+        def before_create
+          token      = Time.now.to_f.to_s.delete(".").to_i
+          self.token = token.to_s(36)
+        end
       end
     end
   end
